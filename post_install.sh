93a6209bc16ef1582c66ee1b6592d675548ead3b
ConorBeh <rogerairgood@gmail.com>
2020-01-22 13:16:55 -0800

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -19,7 +19,7 @@ chown -R git:git /usr/local/share/gitea
 # Set up MySQL database
 # Set variables for username and database name
 DB="gitea"
-USER="dbadmin"
+USER="gitea"
 
 # Save the config values and generate a random password 
 echo "$DB" > /root/dbname
@@ -30,20 +30,9 @@ PASS=`cat /root/dbpassword`
 
 # Configure MySQL
 
-#mysql --protocol=socket -u root <<-EOF
-#CREATE DATABASE ${DB};
-#ALTER USER 'root'@'localhost' IDENTIFIED BY '${PASS}';
-#DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-#DELETE FROM mysql.user WHERE User='';
-#DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
-#CREATE USER '${USER}'@'localhost' IDENTIFIED BY '${PASS}';
-#GRANT ALL PRIVILEGES ON *.* TO '${USER}'@'localhost' WITH GRANT OPTION;
-#GRANT ALL PRIVILEGES ON ${DB}.* TO '${USER}'@'localhost';
-#FLUSH PRIVILEGES;
-#EOF
-
-mysql -u root <<-EOF
-UPDATE mysql.user SET Password=PASSWORD('${PASS}') WHERE User='root';
+mysql --protocol=socket -u root <<-EOF
+CREATE DATABASE ${DB};
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${PASS}';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
@@ -52,6 +41,17 @@ GRANT ALL PRIVILEGES ON *.* TO '${USER}'@'localhost' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON ${DB}.* TO '${USER}'@'localhost';
 FLUSH PRIVILEGES;
 EOF
+
+#mysql -u root <<-EOF
+#UPDATE mysql.user SET Password=PASSWORD('${PASS}') WHERE User='root';
+#DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+#DELETE FROM mysql.user WHERE User='';
+#DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
+#CREATE USER '${USER}'@'localhost' IDENTIFIED BY '${PASS}';
+#GRANT ALL PRIVILEGES ON *.* TO '${USER}'@'localhost' WITH GRANT OPTION;
+#GRANT ALL PRIVILEGES ON ${DB}.* TO '${USER}'@'localhost';
+#FLUSH PRIVILEGES;
+#EOF
 
 # Start service
 service gitea start
