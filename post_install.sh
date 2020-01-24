49f103a427e734dbea00122dc6beb7ff88d0b654
ConorBeh <rogerairgood@gmail.com>
2020-01-23 16:19:25 -0800

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -2,7 +2,7 @@
 
 # Enable service
 sysrc gitea_enable=YES 2>/dev/null
-sysrc mysql_enable=YES 2>/dev/null
+#sysrc mysql_enable=YES 2>/dev/null
 
 # Start/stop service to generate configs
 service gitea start 2>/dev/null
@@ -18,28 +18,28 @@ chown -R git:git /usr/local/share/gitea
 
 # Set up MySQL database
 # Set variables for username and database name
-DB="gitea"
-USER="gitea"
+#DB="gitea"
+#USER="gitea"
 
 # Save the config values and generate a random password 
-echo "$DB" > /root/dbname
-echo "$USER" > /root/dbuser
-export LC_ALL=C
-cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1 > /root/dbpassword
-PASS=`cat /root/dbpassword`
+#echo "$DB" > /root/dbname
+#echo "$USER" > /root/dbuser
+#export LC_ALL=C
+#cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1 > /root/dbpassword
+#PASS=`cat /root/dbpassword`
 
 # Configure MySQL
-mysql -u root <<-EOF
-UPDATE mysql.user SET Password=PASSWORD('${PASS}') WHERE User='root';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
-CREATE USER '${USER}'@'localhost' IDENTIFIED BY '${PASS}';
-CREATE DATABASE ${DB};
-GRANT ALL PRIVILEGES ON *.* TO '${USER}'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON ${DB}.* TO '${USER}'@'localhost';
-FLUSH PRIVILEGES;
-EOF
+#mysql -u root <<-EOF
+#UPDATE mysql.user SET Password=PASSWORD('${PASS}') WHERE User='root';
+#DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+#DELETE FROM mysql.user WHERE User='';
+#DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
+#CREATE USER '${USER}'@'localhost' IDENTIFIED BY '${PASS}';
+#CREATE DATABASE ${DB};
+#GRANT ALL PRIVILEGES ON *.* TO '${USER}'@'localhost' WITH GRANT OPTION;
+#GRANT ALL PRIVILEGES ON ${DB}.* TO '${USER}'@'localhost';
+#FLUSH PRIVILEGES;
+#EOF
 
 # Start service
 service gitea start
@@ -48,16 +48,62 @@ sleep 5
 rm /usr/local/etc/gitea/conf/app.ini 2>/dev/null
 
 # Send database name, login, and password to PLUGIN_INFO
-echo "MySQL Database Name: $DB" > /root/PLUGIN_INFO
-echo "MySQL Database User: $USER" >> /root/PLUGIN_INFO
-echo "MySQL Database Password: $PASS" >> /root/PLUGIN_INFO
-echo "Access the web interface and click "Explore" to start the installer" >> /root/PLUGIN_INFO
+#echo "MySQL Database Name: $DB" > /root/PLUGIN_INFO
+#echo "MySQL Database User: $USER" >> /root/PLUGIN_INFO
+#echo "MySQL Database Password: $PASS" >> /root/PLUGIN_INFO
+#echo "Access the web interface and click "Explore" to start the installer" >> /root/PLUGIN_INFO
 # Output database name, login, and password to UI and remind user where to find it again
-echo "MySQL Database Name: $DB" 
-echo "MySQL Database User: $USER" 
-echo "MySQL Database Password: $PASS" 
-echo "Access the web interface and click Explore to start the installer"
-echo "To view this information again, click Post Install Notes"
+#echo "MySQL Database Name: $DB" 
+#echo "MySQL Database User: $USER" 
+#echo "MySQL Database Password: $PASS" 
+#echo "Access the web interface and click Explore to start the installer"
+#echo "To view this information again, click Post Install Notes"
+
+# Enable the service
+sysrc -f /etc/rc.conf postgresql_enable="YES"
+
+
+# Start the service
+service postgresql initdb 2>/dev/null
+sleep 5
+service postgresql start 2>/dev/null
+sleep 5
+
+USER="pgadmin"
+DB="gitea"
+
+# Save the config values
+echo "$DB" > /root/dbname
+echo "$USER" > /root/dbuser
+export LC_ALL=C
+cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1 > /root/dbpassword
+PASS=`cat /root/dbpassword`
+
+# create user
+psql -d template1 -U postgres -c "CREATE USER ${USER} CREATEDB SUPERUSER;"
+
+# Create production database & grant all privileges on database
+psql -d template1 -U postgres -c "CREATE DATABASE ${DB} OWNER ${USER};"
+
+# Set a password on the postgres account
+psql -d template1 -U postgres -c "ALTER USER ${USER} WITH PASSWORD '${PASS}';"
+
+# Connect as superuser and enable pg_trgm extension
+psql -U postgres -d ${DB} -c "CREATE EXTENSION IF NOT EXISTS pg_trgm;"
+
+# Fix permission for postgres 
+echo "listen_addresses = '*'" >> /var/db/postgres/data11/postgresql.conf
+echo "host  all  all 0.0.0.0/0 md5" >> /var/db/postgres/data11/pg_hba.conf
+
+# Restart postgresql after config change
+service postgresql restart
+sleep 5
+
+echo "Host: localhost" > /root/PLUGIN_INFO
+echo "Database Name: $DB" >> /root/PLUGIN_INFO
+echo "Database User: $USER" >> /root/PLUGIN_INFO
+echo "Database Password: $PASS" >> /root/PLUGIN_INFO
+
 
 
 
